6d29dec347937bde346f45dc1a31398ba7b2807a
TheeValcode <sophiaadesuwa122@gmail.com>
2025-08-18 12:50:31 +0100

    initial commit

---
+++ images/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://apptek.radiantthemes.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://apptek.radiantthemes.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://apptek.radiantthemes.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://apptek.radiantthemes.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://apptek.radiantthemes.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://apptek.radiantthemes.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
